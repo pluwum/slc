8613657e74fb6bda3e3a78177ed66036ca5bda87
Patrick Luwum <luwyxx@gmail.com>
2017-04-18 10:20:22 +0300

    Created function to list prime numbers from 0 to N

--- tdd/primes/primenumbers.py
+++ tdd/primes/primenumbers.py
@@ -7,3 +7,16 @@ def isPrime(number):
             return False
     else:
         return True
+
+
+#return list of primes from 0 to number
+def primes(number):
+	primes = []
+	for x in range(2, number+1):
+		if(isPrime(x)):
+			primes.append(x)
+	return primes	
+
+#mic check 1..2..3
+#print (isPrime(23))
+#print (primes(20))
