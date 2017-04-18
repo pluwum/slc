8ac2de776451f9d8f3bedf27384e7cbcbd0f3020
Patrick Luwum <luwyxx@gmail.com>
2017-04-18 10:59:38 +0300

    Added bigO notation for the  primes function

--- tdd/primes/primenumbers.py
+++ tdd/primes/primenumbers.py
@@ -1,3 +1,9 @@
+#
+#The function can be represented with O(N2)
+#
+#
+
+
 #check if number is a prime
 def isPrime(number):
     if number <= 1:
@@ -9,19 +15,20 @@ def isPrime(number):
         return True
 
 
-#return list of primes from 0 to number
+#return list of primes from 0 to Number
 def primes(number):
-	primes = []
-	if isinstance(number,int):
-		if number >= 0:
-			for x in range(2, number+1):
-				if(isPrime(x)):
-					primes.append(x)
-			return primes
-		else:
-			raise ValueError
-	else:
-		raise TypeError		
+    primes = []
+    if isinstance(number,int):
+        if number >= 0:
+            for x in range(2, number+1):
+                if(isPrime(x)):
+                    primes.append(x)
+            return primes
+        else:
+            raise ValueError
+    else:
+        raise TypeError     
 
 #mic check 1..2..3
+#print (isPrime(23))
 #print (primes(20))
